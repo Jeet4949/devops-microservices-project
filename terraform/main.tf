f71be9d7d5a5c11ce6ba8725252a20cb14ffd5ac
Jeet Hariyani <jeetshariyani12@gmail.com>
2026-03-29 15:50:38 +0530

    main.tf edited

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+# 1. Define the Cloud Provider
+provider "aws" {
+  region = "us-east-1" # Make sure this matches the region you set in aws configure
+}
+
+# 2. Create the Security Group (Firewall)
+resource "aws_security_group" "web_sg" {
+  name        = "terraform-web-sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 3. Dynamically find the latest Ubuntu OS Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# 4. Provision the EC2 Server
+resource "aws_instance" "web_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro" # Free-tier eligible!
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-Automated-Server"
+  }
+}
